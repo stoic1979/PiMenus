4acac531fa089171f9e836dd70e39d62a04204f8
Navjot Singh <weavebytes@gmail.com>
2017-08-04 14:27:01 +0530

    updated param

--- server.py
+++ server.py
@@ -107,4 +107,4 @@ def log():
     return render_template("log.html", **templateData)
 
 if __name__ == '__main__':
-    app.run(debug=True, '0.0.0.0', port=5000, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
